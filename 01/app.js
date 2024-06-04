5551ed1ff80d53e4f53315b56612a6efa1d5f176
Typee8 <wojciechjasiak1324@gmail.com>
2024-06-04 11:55:09 +0200

    Finish 01

--- 01/app.js
+++ 01/app.js
@@ -7,10 +7,33 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+
+    constructor(props) {
+      super(props);
+      console.log("constructor");
+    }
+    
+    componentDidMount() {
+      console.log("componentDidMount");
+      this.id = setInterval(() => {
+        const {counter} = this.state;
+        this.setState({counter: counter + 1});
+      }, 5000);
+    }
+
+    componentDidUpdate() {
+      console.log("componentDidUpdate");
+    }
+
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
+    }
+
+    componentWillUnmount() {
+      console.log("componentWillUnmount");
+      clearInterval(this.id);
     }
 }
 
